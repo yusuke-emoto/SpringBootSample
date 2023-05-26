d5f0b5b55e343f9d7da5d14090546702038cbb79
emoto <h0i8e2n4n@yahoo.co.jp>
2023-05-26 19:58:12 +0900

    6.1.2 メッセージプロパティ

--- src/main/java/com/example/application/service/UserApplicationService.java
+++ src/main/java/com/example/application/service/UserApplicationService.java
@@ -17,10 +17,10 @@ public class UserApplicationService {
     /** 性別のMapを生成する */
     public Map<String, Integer> getGenderMap(/*Locale locale*/) {
         Map<String, Integer> genderMap = new LinkedHashMap<>(); //LinkedHashMapインスタンスを生成して変数genderMapに代入。
-        //String male = messageSource.getMessage("male", null, locale);
-       // String female = messageSource.getMessage("female", null, locale);
-        genderMap.put("男性", 1);  //Map型の変数genderMapにputメソッドを使って、キー名"男性",値を１で登録している。
-        genderMap.put("女性", 2); //Map型の変数genderMapにputメソッドを使って、キー名"女性",値を2で登録している。
+        String male = messageSource.getMessage("male", null, Locale.JAPAN); //MessageSourceのgetMessageメソッドを用いて、messages.propertiesから値を取得して変数maleに代入している
+        String female = messageSource.getMessage("female", null, Locale.JAPAN); //getMessageメソッドの補足説明、第1引数にmessages.propertiesのキー、第2引数に埋め込みパラメーター（動的に設定する値がない場合はnull）、第3引数で国（地域を指定する）
+        genderMap.put(male, 1);  //Map型の変数genderMapにputメソッドを使って、キー名"male",値を１で登録している。
+        genderMap.put(female, 2); //Map型の変数genderMapにputメソッドを使って、キー名"female",値を2で登録している。
         return genderMap; // 22,23行の値を登録した変数genderMapを戻り値として返す。
     }
 }
