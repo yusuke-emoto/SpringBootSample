9b17c793d19d4bdf254b0f8ef2a2427127f39acc
emoto <h0i8e2n4n@yahoo.co.jp>
2023-06-07 21:01:07 +0900

    13.2.2任意のSQL

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.domain.user.model.MUser;
 
@@ -8,4 +11,13 @@ public interface UserRepository extends JpaRepository<MUser, String>{
 	//JPAはjavaの標準のO/RマッパーでSQLを記載しなくても自動で生成してくれる
 	//ここではリポジトリー（保管場所）を生成している
 	//JPAでリポジトリーを作成する場合、JpaRepositoryを継承する必要がある
+	
+	/** ログインユーザー検索 */
+	@Query("select user" + " from MUser user" + " where userId = :userId")//@Queryをつけると任意のSQLを実行できる
+	public MUser findLoginUser(@Param("userId") String userId);//@Paramを使うとメソッドの引数の名称を変えることができる
+	
+	/** ユーザー更新 */
+	@Modifying//insertとupdateとdeleteを実行する場合、@Modifyingをつけないといけない
+	@Query("update MUser" + " set" + " password = :password" + " , userName = :userName" + " where" + " userId = :userId")
+	public Integer updateUser(@Param("userId") String userId, @Param("password") String password, @Param("userName") String userName);
 }
